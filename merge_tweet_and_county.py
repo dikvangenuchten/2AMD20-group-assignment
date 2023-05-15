2490b8df97188558f0e0c7d6982e89d83ed68ea1
Dik van Genuchten <h.j.m.v.genuchten@student.tue.nl>
2023-05-15 10:37:06 +0200

    Optimizations (32h to 3m)

--- merge_tweet_and_county.py
+++ merge_tweet_and_county.py
@@ -1,10 +1,12 @@
+import numpy as np
 import pandas as pd
 from functools import partial
-from geopy.distance import distance as geo_distance
+
+import tqdm
 from tqdm.contrib.concurrent import process_map
 
 
-def add_closest_county(row, all_counties):
+def find_closest_county(row, county_points: np.ndarray, county_names: list):
     tweet = row[1]
     if (
         tweet["lat"] is None
@@ -12,13 +14,15 @@ def add_closest_county(row, all_counties):
         or pd.isna(tweet["lat"])
         or pd.isna(tweet["long"])
     ):
+        # No location information
         return None
 
     lat, lon = tweet["lat"], tweet["long"]
-    dist, county = min(
-        (geo_distance((lat, lon), coord), county)
-        for county, coord in all_counties.items()
-    )
+
+    distances = calculate_distance_vectorized((lat, lon), county_points)
+    closest_idx = np.argmin(distances)
+    dist = distances[closest_idx]
+    county = county_names[closest_idx]
 
     if dist > 100:
         # Most likely not an american tweeting
@@ -27,19 +31,48 @@ def add_closest_county(row, all_counties):
     return county
 
 
-def main(name: str, counties: dict):
+def calculate_distance_vectorized(point: np.ndarray, county_points: np.ndarray) -> int:
+    """Calculates the distance on a sphere in kilometers.
+
+    Using the haversine formula.
+    This is an approximation as the world is not a perfect sphere
+    """
+    radius = 6371  # kilometers
+
+    lat1 = point[0]
+    lon1 = point[1]
+    lat2 = county_points[:, 0]
+    lon2 = county_points[:, 1]
+
+    phi1 = lat1 * np.pi / 180
+    phi2 = lat2 * np.pi / 180
+    delta_phi = (lat2 - lat1) * np.pi / 180
+
+    delta_lmd = (lon2 - lon1) * np.pi / 180
+    a = np.sin(delta_phi / 2) * np.sin(delta_phi / 2) + np.cos(phi1) * np.cos(
+        phi2
+    ) * np.sin(delta_lmd / 2) * np.sin(delta_lmd / 2)
+    c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1 - a))
+    return radius * c
+
+
+def main(name: str, counties: pd.DataFrame):
     print(f"Starting: {name}")
     df_tweets = pd.read_csv(
         f"datasets/tweets/hashtag_{name}.csv",
         lineterminator="\n",
     )
+    county_names = list(counties["county"])
+    county_points = np.asarray(counties[["lat", "lng"]])
     print("Loaded data")
-    result = process_map(
-        partial(add_closest_county, all_counties=counties),
-        df_tweets.iterrows(),
-        chunksize=100,
-        total=df_tweets.shape[0]
-    )
+
+    result = [
+        find_closest_county(row, county_points, county_names)
+        for row in tqdm.tqdm(
+            df_tweets[["lat", "long"]].iterrows(), total=df_tweets.shape[0]
+        )
+    ]
+
     print("Processed data")
     df_tweets["county"] = pd.Series(result)
     df_tweets.to_csv(
@@ -50,10 +83,9 @@ def main(name: str, counties: dict):
 
 
 if __name__ == "__main__":
-    df_counties = pd.read_csv("datasets/uscounties/uscounties.csv")
-    county_dict = {
-        x.county: (x.lat, x.lng)
-        for x in df_counties[["county", "lat", "lng"]].itertuples()
-    }
-    main("donaldtrump", county_dict)
-    main("joebiden", county_dict)
+    df_counties = pd.read_csv("datasets/uscounties/uscounties.csv")[
+        ["county", "lat", "lng"]
+    ]
+
+    main("donaldtrump", df_counties)
+    main("joebiden", df_counties)
