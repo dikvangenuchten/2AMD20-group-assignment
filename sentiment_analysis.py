0f6301b7e05e9de68a935b415cefe121083ca15f
dik <h.j.m.v.genuchten@student.tue.nl>
2023-06-05 13:59:15 +0200

    Clean up

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,7 +1,7 @@
 import pandas as pd
-import copy
+
 from langdetect import detect
-import numpy as np
+
 from tqdm import tqdm
 
 from functools import partial
@@ -9,10 +9,10 @@ from functools import partial
 import nltk
 
 nltk.download("vader_lexicon")
-
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 tqdm.pandas()
+
 
 def language(x):
     try:
@@ -21,7 +21,7 @@ def language(x):
         return None
 
 
-def vader_sentiment(sid, tweet):
+def vader_sentiment(tweet, sid):
     try:
         return sid.polarity_scores(tweet)
     except:
@@ -32,35 +32,38 @@ def output(vader_dict):
     # Nonetype threw an error (while I filter those out later) so I use another try except here
     try:
         polarity = "neutral"
-        if (vader_dict["compound"] >= 0.05):
+        if vader_dict["compound"] >= 0.05:
             polarity = "positive"
-        elif (vader_dict["compound"] <= -0.05):
+        elif vader_dict["compound"] <= -0.05:
             polarity = "negative"
 
         return polarity
     except:
         return None
 
+
 def main(name: str = "joebiden"):
     sid = SentimentIntensityAnalyzer()
 
     df = pd.read_csv(f"datasets/tweets/hashtag_{name}.csv")
 
-    df['language'] = df['tweet'].progress_apply(language)
+    df["language"] = df["tweet"].progress_apply(language)
     df.to_csv(f"{name}_tweet_with_language.csv")
 
     # Filter on only English for sentiment analysis, use original dataset for further visualisation
-    df_en = df[df['language'] == 'en']
-    print(len(df_en)) # Keep track how many were filtered out
+    df_en = df[df["language"] == "en"]
+    print(len(df_en))  # Keep track how many were filtered out
 
-    df_en['compound'] = df_en['tweet'].progress_apply(partial(vader_sentiment, sid=sid))
+    df_en["compound"] = df_en["tweet"].progress_apply(partial(vader_sentiment, sid=sid))
     # Filter out Nonevalues since not needed for analysis
-    df_en = df_en[df_en['compound'] != None]
-    print(len(df_en)) # Keep track how many were filtered out
+    df_en = df_en[df_en["compound"] != None]
+    # Keep track how many were filtered out
+    print(len(df_en))
 
-    df_en['emotion'] = df_en['compound'].progress_apply(output)
+    df_en["emotion"] = df_en["compound"].progress_apply(output)
 
     df_en.to_csv(f"{name}_english_tweets_with_compound.csv")
+
 
 if __name__ == "__main__":
     main("joebiden")
